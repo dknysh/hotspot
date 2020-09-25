c0d24c045945f0d701d42da043578653572a6139
dknysh <d.knysh@nips.ru>
2020-09-25 18:43:51 +0700

    Added some settings to Cross-profiling settings dialog.
    Settings to 'Override with currently opened perf data path'.
    'Target Root' for path to the copy of Target file system structure on Host machine.
    Processing of targetRoot on Disassembly side.

--- src/parsers/perf/perfparser.cpp
+++ src/parsers/perf/perfparser.cpp
@@ -1321,7 +1321,8 @@ PerfParser::~PerfParser() = default;
 
 void PerfParser::startParseFile(const QString& path, const QString& sysroot, const QString& kallsyms,
                                 const QString& debugPaths, const QString& extraLibPaths, const QString& appPath,
-                                const QString& arch, const QString& disasmApproach, const QString& maxStack)
+                                const QString& targetRoot, const QString& arch, const QString& disasmApproach,
+                                const QString& maxStack)
 {
     Q_ASSERT(!m_isParsing);
 
@@ -1376,7 +1377,7 @@ void PerfParser::startParseFile(const QString& path, const QString& sysroot, con
     m_callerCalleeResults = {};
     m_events = {};
     m_disassemblyResult = {};
-    m_disassemblyResult.setData(path, appPath, extraLibPaths, arch, disasmApproach);
+    m_disassemblyResult.setData(path, appPath, targetRoot, extraLibPaths, arch, disasmApproach);
 
     emit parsingStarted();
     using namespace ThreadWeaver;
